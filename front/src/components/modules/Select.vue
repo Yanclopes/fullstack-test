a3c5a9ffe802e7b64116eac3f46113d77c58e166
yan <yanclopes08@gmail.com>
2024-01-17 15:41:03 -0300

    Create select comp

---
+++ front/src/components/modules/Select.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue';
+import {getStates} from '../../services/GetStates.js'
+
+const states = ref([]);
+const cities = ref([]);
+const loading = ref(false);
+
+async function loadStates() {
+  states.value = await getStates();
+}
+
+async function loadCities(state) {
+  loading.value = true;
+  cities.value = await getCities(state);
+  loading.value = false;
+}
+loadStates();
+
+async function changeState(event){
+  loadCities(event.target.value)
+}
+
+async function changeCitie(event){
+  this.$emit('option-selected', event.target.value);
+}
+</script>
+
+<template>
+  <div>
+    <div>
+      <select @change="changeState">
+        <option v-for="state in states" :key="state.id" :value="state.name">{{ state.name }}</option>
+      </select>
+    </div>
+    <div>
+      <div v-if="loading">Carregando...</div>
+      <select v-else @change="changeCitie">
+        <option v-for="city in cities" :key="city.id" :value="city.name">{{ city.name }}</option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
