6bba5e7011da981cbd190929af4f6bee2b65f60b
yan <yanclopes08@gmail.com>
2024-01-17 15:42:54 -0300

    developing App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,9 +1,28 @@
-<script setup>
+<script>
 
+import Select from "@/components/modules/Select.vue";
+import Card from "@/components/modules/Card.vue";
+
+export default {
+  components: { Select, Card },
+  data() {
+    return {
+      selectedOption:
+    }
+  },
+  methods: {
+    handleOptionSelected(option) {
+      this.selectedOption = option;
+    }
+  }
+}
 </script>
 
 <template>
-
+  <div>
+    <Select @option-selected="handleOptionSelected" />
+    <Card :selectedOption="selectedOption"/>
+  </div>
 </template>
 
 <style scoped>
