8fbef3b328762a73e84e54abbbc806035954a5fa
Yan <yanclpoes08@gmail.com>
2024-01-20 20:20:49 -0300

    ajustando servidor de produção

--- frontEnd/src/services/GetWeather.js
+++ frontEnd/src/services/GetWeather.js
@@ -2,11 +2,9 @@ import {http} from './index.js'
 export async function getWeather(city) {
     return http.get(`/weather?city=${city}`)
         .then(response => {
-            console.log(response)
             return response.data;
         })
         .catch(error => {
-            console.log(error)
             if (error.response) {
                 return { error: true, message: 'Erro de resposta do servidor' };
             } else if (error.request) {
